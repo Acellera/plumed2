a47752c2261451316787fe9dc3af5ab8e29beaae
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 13:28:21 +0100

    use gcc-4.4

--- package/build.sh
+++ package/build.sh
@@ -4,10 +4,10 @@ mkdir -p "$DIR/bin/"
 
 printenv
 
-./configure --prefix=$DIR --disable-mpi
-make -j 4
+./configure --prefix=$DIR --disable-mpi CC=gcc-4.4 CXX=g++-4.4 FC=gfortran-4.4
+make -j 4 
 cd acemd
-make
+make CC=gcc-4.4
 
 cp libplumed2.so libplumedKernel.so libplumed2plugin.so  "$DIR/lib"
 
