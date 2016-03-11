d8eec729daaf85580c40f68c38b36a47c3624cf2
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 21:38:55 +0100

    plumed libc wrapper

--- package/build.sh
+++ package/build.sh
@@ -13,7 +13,10 @@ make -j 4
 cd acemd
 make CC=$CC TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" 
 
-cp ../src/lib/install/plumed "$DIR/bin"
+cp ../src/lib/install/plumed "$DIR/bin/plumed.bin"
+echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/.lib/compat-libc" "$DIR/plumed.bin" $@' > "$DIR/bin/plumed"
+chmod +x "$DIR/bin/plumed"
+
 cp libplumed2.so libplumedKernel.so libplumed2plugin.so  "$DIR/lib"
 
 
