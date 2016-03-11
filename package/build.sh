a29b559b1aa33966cba40822cb87b809687158b2
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 14:11:08 +0100

    rev build

--- package/build.sh
+++ package/build.sh
@@ -11,7 +11,8 @@ printenv
 ./configure --prefix=$DIR --disable-mpi CC=$CC CXX=$CXX FC=$FC
 make -j 4 
 cd acemd
-make CC=$CC
+make CC=$CC TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" 
+
 
 cp libplumed2.so libplumedKernel.so libplumed2plugin.so  "$DIR/lib"
 
