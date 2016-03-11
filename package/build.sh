9ae914813b5180b017d32bd7df5c6590244c20fe
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 15:44:24 +0100

    rev build add binary

--- package/build.sh
+++ package/build.sh
@@ -13,7 +13,7 @@ make -j 4
 cd acemd
 make CC=$CC TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" 
 
-
+cp ../src/lib/install/plumed "$DIR/bin"
 cp libplumed2.so libplumedKernel.so libplumed2plugin.so  "$DIR/lib"
 
 
