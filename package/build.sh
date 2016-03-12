37e39def197951abc9f0e50fc94aadf0aa5d7766
M J Harvey <m.j.harvey@acellera.com>
2016-03-12 10:39:33 +0100

    rev

--- package/build.sh
+++ package/build.sh
@@ -14,7 +14,7 @@ cd acemd
 make CC=$CC TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" 
 
 cp ../src/lib/install/plumed "$DIR/bin/plumed.bin"
-echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/.lib/compat-libc" "$DIR/plumed.bin" $@' > "$DIR/bin/plumed"
+echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/../lib/compat-libc" "$DIR/plumed.bin" $@' > "$DIR/bin/plumed"
 chmod +x "$DIR/bin/plumed"
 
 cp libplumed2.so libplumedKernel.so libplumed2plugin.so  "$DIR/lib"
