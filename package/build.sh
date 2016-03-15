e72bfa088a8bfbb42fc29005c5377317e1748323
M J Harvey <m.j.harvey@acellera.com>
2016-03-15 22:01:05 +0100

    diddle

--- package/build.sh
+++ package/build.sh
@@ -12,9 +12,11 @@ printenv
 make -j 4 
 
 
-  cd ../ && make -j 6 && cd - && cp ..//src/lib/install/libplumed.so libplumed2.so && cp ..//src/lib/install/libplumedKernel.so .
-cp src/lib/libplumed.dylib  acemd/libplumed2.so
-cp src/lib/install/libplumedKernel.dylib acemd/libplumedKernel.so
+ls -l src/lib/install
+
+
+cp src/lib/install/libplumed.* acemd/libplumed2.so 
+cp src/lib/install/libplumedKernel.* acemd/libplumedKernel.so
  
 cd acemd
 make CC=$CC TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" 
@@ -25,11 +27,13 @@ echo 'DIR=$(dirname "$(which python)"); LD_LIBRARY_PATH="$DIR/../lib/compat-libc
 chmod +x "$DIR/bin/plumed"
 
 cp libplumed2.so libplumedKernel.so libplumed2plugin.so  "$DIR/lib"
-cd "$DIR/lib"
-ln -s libplumed2.so libplumed2.dylib
-ln -s libplumedKernel.so libplumedKernel.dylib
-ln -s libplumed2plugin.so libplumed2plugin.dylib
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+	cd "$DIR/lib"
+	ln -s libplumed2.so libplumed2.dylib
+	ln -s libplumedKernel.so libplumedKernel.dylib
+	ln -s libplumed2plugin.so libplumed2plugin.dylib
+fi
 
 
 
