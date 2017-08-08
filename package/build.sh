e4b77f728dbd3d481372213061170b31f4196d95
Toni G <toni.giorgino@gmail.com>
2017-08-08 15:10:06 +0200

    osx

--- package/build.sh
+++ package/build.sh
@@ -8,7 +8,7 @@ make -j 2 > log 2>&1
 make install
 
 # A compatibility symlink until  acemd3 and local openmm-plumed are updated.
-ln -s libplumed.so $PREFIX/lib/libplumed2.so
+ln -s libplumed.so $PREFIX/lib/libplumed2.so || ln -s libplumed.dylib $PREFIX/lib/libplumed2.dylib 
 
 # Build and install acemd's plugin
 cp -r $RECIPE_DIR/acemd .
