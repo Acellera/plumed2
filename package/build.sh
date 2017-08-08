5cc5c0e4de3c24a9cf01217bddb25949844ab1ac
Toni G <toni.giorgino@gmail.com>
2017-08-08 14:50:20 +0200

    readded plumed2.so as a symlink

--- package/build.sh
+++ package/build.sh
@@ -2,10 +2,15 @@
 
 printenv
 
+# Standard plumed2 build+install
 ./configure --prefix=$PREFIX --disable-mpi
 make -j 2 > log 2>&1
 make install
 
+# A compatibility symlink until  acemd3 and local openmm-plumed are updated.
+ln -s libplumed.so $PREFIX/lib/libplumed2.so
+
+# Build and install acemd's plugin
 cp -r $RECIPE_DIR/acemd .
 cd acemd
 make
