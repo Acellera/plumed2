2f19d060765720159af779f78ca9a0b45581a46b
Raimondas Galvelis <r.galvelis@acellera.com>
2017-11-17 14:48:03 +0100

    Modernize acemd2_plugin source fetching in conda build

--- package/build.sh
+++ package/build.sh
@@ -10,8 +10,7 @@ make install
 # A compatibility symlink until  acemd3 and local openmm-plumed are updated.
 ln -s libplumed.so $PREFIX/lib/libplumed2.so || ln -s libplumed.dylib $PREFIX/lib/libplumed2.dylib
 
-# Build and install acemd's plugin
-cp -r $RECIPE_DIR/../acemd2_plugin .
+# Build and install ACEMD2 plugin
 cd acemd2_plugin
 make
 cp libplumed2plugin.so $PREFIX/lib
