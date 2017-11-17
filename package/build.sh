6768166f4c03c773c88d41c541834482023435bc
Raimondas Galvelis <r.galvelis@acellera.com>
2017-11-17 11:55:04 +0100

    Move away acemd2_plugin not to include source in the conda package

--- package/build.sh
+++ package/build.sh
@@ -8,11 +8,11 @@ make -j 2 > log 2>&1
 make install
 
 # A compatibility symlink until  acemd3 and local openmm-plumed are updated.
-ln -s libplumed.so $PREFIX/lib/libplumed2.so || ln -s libplumed.dylib $PREFIX/lib/libplumed2.dylib 
+ln -s libplumed.so $PREFIX/lib/libplumed2.so || ln -s libplumed.dylib $PREFIX/lib/libplumed2.dylib
 
 # Build and install acemd's plugin
-cp -r $RECIPE_DIR/acemd .
-cd acemd
+cp -r $RECIPE_DIR/../acemd2_plugin .
+cd acemd2_plugin
 make
 cp libplumed2plugin.so $PREFIX/lib
 
