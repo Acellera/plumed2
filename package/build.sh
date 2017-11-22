0f816e5acb2eb041767dedc130714c49b645371d
Toni G <toni.giorgino@gmail.com>
2017-11-22 16:06:47 +0100

    Readded symlink

--- package/build.sh
+++ package/build.sh
@@ -8,7 +8,7 @@ make -j $CPU_COUNT
 make install
 
 # A compatibility symlink until  acemd3 and local openmm-plumed are updated.
-# ln -s libplumed.so $PREFIX/lib/libplumed2.so || ln -s libplumed.dylib $PREFIX/lib/libplumed2.dylib
+ln -s libplumed.so $PREFIX/lib/libplumed2.so || ln -s libplumed.dylib $PREFIX/lib/libplumed2.dylib
 
 # Build and install ACEMD2 plugin
 cd acemd2_plugin
